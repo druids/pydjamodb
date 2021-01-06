ee4b67f7bbcb2e788d0dda8bd2c28a6c86e128b5
Lubos Matl <lubos.matl@mallpay.cz>
2021-01-06 21:02:32 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydjamodb',
-    version='0.0.2',
+    version='0.0.3',
     description="Django interface to PyDjamoDB.",
     keywords='django, DynamoDB, PyDjamoDB',
     author='Lubos Matl',
